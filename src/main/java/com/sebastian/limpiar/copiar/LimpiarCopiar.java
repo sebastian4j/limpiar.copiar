5ea8f22775c641e55f2bbb848d4126b257db6ca3
Sebastián Ávila A <sebastian4j@gmail.com>
2019-05-06 22:58:27 -0400

    Initial commit

---
+++ src/main/java/com/sebastian/limpiar/copiar/LimpiarCopiar.java
@@ -0,0 +1,17 @@
+package com.sebastian.limpiar.copiar;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * demo para generar un jar en el target, limpiar el contenido y copiar a otra
+ * carpeta de destino.
+ *
+ * @author Sebastián Ávila A.
+ */
+public class LimpiarCopiar {
+    private static final Logger LOGGER = LogManager.getLogger(LimpiarCopiar.class);
+    public static void main(String[] args) {
+        LOGGER.info("hola");
+    }
+}
